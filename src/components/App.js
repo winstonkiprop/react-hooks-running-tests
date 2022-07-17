aabdf39d1b942b40078fb34636156577ba544dcc
winston kiprop <winstonekip50@gmail.com>
2022-07-17 23:04:01 +0300

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Article from './Article'
+import Article from "./Article.1";
 import Header from './Header'
 
 function App() {
--- src/components/Article.1.js
+++ src/components/Article.1.js
@@ -1,13 +1,11 @@
-import React from "react";
-
 function Article() {
   return (
-    <div> 
+    <div>
+      {/* please pass this test */}
        please pass this test 
     </div>
+    
   );
+  
 }
-
-
-export default Article; 
-
+export default Article;
